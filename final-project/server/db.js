f572a5a5b3232c9541d83a832755a307c7cf7a2b
Saqibkhan566 <saqibkhan93000@gmail.com>
2025-05-24 15:52:50 +0500

    update db

--- final-project/server/db.js
+++ final-project/server/db.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI = "mongodb://localhost:27017/";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
     const count = retryCount ?? 0;
     try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
+        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1' });
         console.info('Connected to Mongo Successfully')
 
         return;
