bdb9b9c453f0688bd2d698e2ad5b4abe89b0b27c
Saqibkhan566 <saqibkhan93000@gmail.com>
2025-06-01 10:22:06 +0500

    update index.js

--- final-project/server/index.js
+++ final-project/server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
 const connectToMongo = require('./db');
 const app = express();
-const path = require('path');
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 const PORT = process.env.PORT || 8181;
 
 
@@ -16,11 +19,6 @@ connectToMongo();
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
